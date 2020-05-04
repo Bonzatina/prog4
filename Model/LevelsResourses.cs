7f449dcca39e440afd4cfa8da970d8fbcf61120e
bonzatina <martymckul@gmail.com>
2020-05-04 22:09:55 +0200

    made GameShapes class static

--- Model/LevelsResourses.cs
+++ Model/LevelsResourses.cs
@@ -28,21 +28,20 @@ namespace Model
             };
             Point[][] grounds = new Point[][] { oneSlice, secondSlice };
 
-            GameShapes gameShapesClass = new GameShapes();
-
+            
             List<SpecialItem> specialItems = new List<SpecialItem>();
             //specialItems.Add(new SpecialItem(0, GameModel.ZeroAxios, Brushes.Green, new Pen(Brushes.Black, 2), new RectangleGeometry(new Rect(220, -100, 20, 30))));
-            specialItems.Add(new IncreaseHealthItem(1, 220, GameModel.ZeroAxios, Brushes.Red, new Pen(Brushes.Black, 2), gameShapesClass.gameShapes["plusOneLiveShape"]));
-            specialItems.Add(new DecreaseHealthItem(1, 700, GameModel.ZeroAxios, Brushes.Red, new Pen(Brushes.Black, 2), gameShapesClass.gameShapes["spikesShape"].GetWidenedPathGeometry(new Pen(Brushes.Black, 2))));
+            specialItems.Add(new IncreaseHealthItem(1, 220, GameModel.ZeroAxios, Brushes.Red, new Pen(Brushes.Black, 2), GameShapes.gameShapes["plusOneLiveShape"]));
+            specialItems.Add(new DecreaseHealthItem(1, 700, GameModel.ZeroAxios, Brushes.Red, new Pen(Brushes.Black, 2), GameShapes.gameShapes["spikesShape"].GetWidenedPathGeometry(new Pen(Brushes.Black, 2))));
 
             levels.Add("level_1", new Level(new GroundLine(0, GameModel.ZeroAxios, grounds), specialItems));
         }
     }
 
-    public  class GameShapes
+    public static  class GameShapes
     {
-        public  Dictionary<string, Geometry> gameShapes = new Dictionary<string, Geometry>();
-        public GameShapes()
+        public static  Dictionary<string, Geometry> gameShapes = new Dictionary<string, Geometry>();
+        static GameShapes()
         {
             GeometryGroup spikesShape = new GeometryGroup();
             spikesShape.Children.Add(new LineGeometry(new Point(0, -80), new Point(5, -60)));
@@ -51,10 +50,10 @@ namespace Model
             spikesShape.Children.Add(new LineGeometry(new Point(15, -60), new Point(20, -80)));
             spikesShape.Children.Add(new LineGeometry(new Point(20, -80), new Point(25, -60)));
             spikesShape.Children.Add(new LineGeometry(new Point(25, -60), new Point(30, -80)));
-            this.gameShapes.Add("spikesShape", spikesShape);
+            gameShapes.Add("spikesShape", spikesShape);
 
             Geometry plusOneLiveShape = new EllipseGeometry(new Rect(0, -100, 20, 30));
-            this.gameShapes.Add("plusOneLiveShape", plusOneLiveShape);
+            gameShapes.Add("plusOneLiveShape", plusOneLiveShape);
         }
     }
 }
