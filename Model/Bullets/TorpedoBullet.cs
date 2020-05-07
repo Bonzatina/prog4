b5de65f3213090a80783e42d3203e8bf3bb3e39a
Kőhegyi Marcell <wondr.waffl@gmail.com>
2020-05-07 22:14:20 +0200

    Recreated the bullets from the previously failed bulletsBackend branch.

---
+++ Model/Bullets/TorpedoBullet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Bullets
+{
+    public class TorpedoBullet : Bullet
+    {
+        public TorpedoBullet(double cx, double cy) : base(cx, cy, 100)
+        {
+
+        }
+    }
+}
