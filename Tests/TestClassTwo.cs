cef657b3669c59de5c739b52b1f6a78b63d2c19c
arcthiccfox <wondr.waffl@gmail.com>
2020-05-15 01:38:04 +0200

    Create tests for adding to enemies list

--- Tests/TestClassTwo.cs
+++ Tests/TestClassTwo.cs
@@ -1,13 +1,27 @@
-﻿using System;
+﻿using Logic;
+using Model;
+using Model.Enemies;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace Tests
 {
-    class TestClassTwo
-    {
 
+    public class TestClassTwo
+    {
+        
+        [Test]
+        public void GivenNoEnemiesExist_AndAddingAnEnemy_ListIsLargerThanZero()
+        {
+            GameModel model = new GameModel(1, 1);
+            GameLogic logic = new GameLogic(model);
+            logic.AddEnemy(new BossEnemy(Geometry.Empty));
+            Assert.That(logic.GetAllEnemies().Count>0, Is.True);
+        }
     }
 }
